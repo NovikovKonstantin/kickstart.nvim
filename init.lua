a7ad885ab8d8138549b7b3f9662195fa5180ff8f
NovikovKonstantin <novikov.konstantin.nk@gmail.com>
2024-07-04 04:08:54 +0300

    Removed commenting plugin
    This functionality is builtin now

--- init.lua
+++ init.lua
@@ -131,9 +131,6 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
-  -- "gc" to comment visual regions/lines
-  'numToStr/Comment.nvim',
-
   -- Adds git related signs to the gutter, as well as utilities for managing changes
   {
     'lewis6991/gitsigns.nvim',

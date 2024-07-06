255245771c065a876f965a30d20ac8b553186c7a
NovikovKonstantin <novikov.konstantin.nk@gmail.com>
2024-07-06 08:58:12 +0300

    Add autopairs

--- init.lua
+++ init.lua
@@ -456,6 +456,20 @@ require('lazy').setup({
     end,
   },
 
+  -- Autopairs
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    -- Optional dependency
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require('nvim-autopairs').setup {}
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
+  },
+
   -- Colorscheme
   {
     'catppuccin/nvim',
